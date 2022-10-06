863d939ccef88b97d136d81c4bd289b3926d424b
Alejandro Migueles <alexmigueles@gmail.com>
2022-10-06 17:10:01 -0500

    Se añaden estilos al producto individual

--- src/pages/SingleItem.jsx
+++ src/pages/SingleItem.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import fetchProductItem from '../hooks/fetchProductItem'
+import '../pages/pagesStyles/SingleItem.scss'
 
 const SingleItem = () => {
   const { id } = useParams()
   const dataItem = fetchProductItem(id)
   // console.log(dataItem.productSingleItem)
   return (
-    <div>
-      SingledcdItem
-      <div>
-        <h1>{dataItem.productSingleItem.product_name}</h1>
-        <h4>{dataItem.productSingleItem.brand}</h4>
-        <p>{dataItem.productSingleItem.price}</p>
-        <img src={dataItem.productSingleItem.image} alt='' />
-        <p>{dataItem.productSingleItem.description}</p>
-      </div>
+    <div className='center'>
+      <section className='product'>
+        <div className='images'>
+          <img src={dataItem.productSingleItem.image} alt='' width='250px' />
+        </div>
+        <div className='product_data'>
+          <h1 className='product_name'>{dataItem.productSingleItem.product_name}</h1>
+          <div className='subtitle'>
+            <p className='product_brand'>{dataItem.productSingleItem.brand}</p>
+            <p className='price'>${dataItem.productSingleItem.price}</p>
+          </div>
+          <div className='description rte'>
+            <p>{dataItem.productSingleItem.description}</p>
+          </div>
+        </div>
+      </section>
     </div>
 
   )
