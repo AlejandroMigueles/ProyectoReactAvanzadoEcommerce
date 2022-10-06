5d17361c196d920ae21dcaf528398abde36aad94
Alejandro Migueles <alexmigueles@gmail.com>
2022-10-06 01:35:37 -0500

    Feat: Se añaden rutas a productos indivituales

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,23 @@
 /* eslint-disable no-undef */
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Card.scss'
 
 const Card = ({ id, title, price, image }) => {
   return (
-    <div className='container col-md-3 mb-2 p-2 '>
-      <div className='card box2' style={{ width: '18rem' }}>
-        <img src={image} className='card-img-top' alt='...' height={300} />
-        <div className='card-body'>
-          <h5 className='card-title'>{title}</h5>
-          <p className='card-text'>${price}</p>
+    <Link to={`product/${id}`} style={{ color: 'inherit', textDecoration: 'inherit' }} className='container col-md-3 mb-2 p-2 '>
+      <article>
+        <div>
+          <div className='card box2' style={{ width: '18rem' }}>
+            <img src={image} className='card-img-top' alt='...' height={300} />
+            <div className='card-body'>
+              <h5 className='card-title'>{title}</h5>
+              <p className='card-text'>${price}</p>
+            </div>
+          </div>
         </div>
-      </div>
-
-    </div>
+      </article>
+    </Link>
   )
 }
 
--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -3,6 +3,7 @@ import { useContext } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Home, Login, Secret, Signup, Dashboard } from '@/pages'
 import { AuthContext } from '../context/AuthContext'
+import { SingleItem } from '@/components/SingleItem'
 
 const RoutesIndex = () => {
   const { isAuth } = useContext(AuthContext)
@@ -17,6 +18,7 @@ const RoutesIndex = () => {
         path='/dashboard'
         element={isAuth ? <Dashboard /> : <Navigate to='/login' replace />}
       />
+      <Route path='/product/:id' element={SingleItem} />
     </Routes>
 
   )
